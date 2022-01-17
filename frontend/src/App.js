5792f4bb4e95be4d4443e37fb45435ea056b22c1
WWeerawat <weerawat.fo@mail.kmutt.ac.th>
2022-01-17 21:53:23 +0700

    Modify | Setup yarn to react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </header>
+      <div class="sm:container sm:mx-auto">
+        <img
+          class="object-fill "
+          src="https://media.istockphoto.com/photos/aerial-view-of-land-and-positioning-point-area-picture-id1254330782?k=20&m=1254330782&s=612x612&w=0&h=41xlEGuv95_fEDp1euilGldyDYSArI5VaIjPXMbWbEE="
+          alt="headerImage"
+        />
+      </div>
     </div>
   );
 }
