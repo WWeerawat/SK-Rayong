df6eee0af6d69e22f94b3479cf80882729afab72
WWeerawat <weerawat.fo@mail.kmutt.ac.th>
2022-01-22 02:17:14 +0700

    Add | Fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,7 +11,26 @@ module.exports = {
         "2xl": "6rem",
       },
     },
-    extend: {},
+    fontFamily: {
+      // Note: This is @notapatch and not the docs
+      //       I think what it is trying to say is that if you define
+      //       a custom font here you are also removing the default
+      //       font families sans, serif and mono.
+      //
+
+      display: ["Noto Sans Thai", "sans-serif"],
+      body: ['"Open Sans"', "serif"],
+    },
+    extend: {
+      colors: {
+        primary: "#067C3D",
+        secondary1: "#3C9C54",
+        secondary2: "#569242",
+        secondary3: "#D8F0C8",
+        secondary4: "#F7E630",
+        body: "#777070",
+      },
+    },
   },
   plugins: [],
 };
