30198f553d24852db5643708293790d4bcba2e9c
WWeerawat <weerawat.fo@mail.kmutt.ac.th>
2022-02-18 17:00:00 +0700

    add staticfile fdir

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,9 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-# STATICFILES_DIRS = (
-#     PROJECT_DIR / "frontend" / "build" / "static",  # update the STATICFILES_DIRS
-# )
+STATICFILES_DIRS = (BASE_DIR / "static",)  # update the STATICFILES_DIRS
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
