87b03adec5cafe91e2c685557e0f97625cfbf4f3
WWeerawat <weerawat.fo@mail.kmutt.ac.th>
2022-02-18 16:22:16 +0700

    make it better

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -91,7 +92,8 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
---
+++ backend/release.sh
@@ -0,0 +1,3 @@
+#l/usr/bin/env bash
+
+python manage.py migrate --no-input
